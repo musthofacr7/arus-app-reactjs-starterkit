73302ebf9c2843ba3c49a1066156c79106af8b3f
Yuan Virna <yuvirna93@gmail.com>
2019-11-20 00:49:32 +0700

    fix splash screen

--- arus/src/pages/splashScreen/style.js
+++ arus/src/pages/splashScreen/style.js
@@ -1,2 +1,16 @@
-const Style = {};
+const Style = {
+  Container: {
+    width: '100%',
+    height: '100vh',
+    backgroundSize: 'cover',
+    flexDirection: 'column',
+    display: 'flex',
+    overflowY: 'hidden',
+    overflowX: 'hidden',
+    flex: '1',
+    backgroundColor: '#26CAC0',
+    // margin: 0
+    padding: 0
+  }
+};
 export default Style;

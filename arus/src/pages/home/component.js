77475f0ea35384c0a233df8ecd5eec119ad75e26
malichalaelar <maalicha.maalicha@gmail.com>
2019-11-21 08:21:19 +0700

    fix

--- arus/src/pages/home/component.js
+++ arus/src/pages/home/component.js
@@ -1,15 +1,31 @@
 import React from "react";
-import Appbar from "../../component/appbar";
 import BottomNavigation from "../../component/bottom-navigation";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
 
 class component extends React.Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <Appbar title="home" />
+      <Container maxWidth="xs" className={classes.container}>
+        <Grid className={classes.gridContainer}>
+          <Grid item xs={12} className={classes.gridTop}>
+            <Grid container spacing={0} className={classes.gridProfil}>
+              <Grid item xs={6}>
+                hello
+              </Grid>
+              <Grid item xs={6}>
+                hello
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} className={classes.gridBottom}>
+            hello
+          </Grid>
+        </Grid>
 
         <BottomNavigation />
-      </div>
+      </Container>
     );
   }
 }
--- arus/src/pages/home/style.js
+++ arus/src/pages/home/style.js
@@ -1,2 +1,33 @@
-const Style = {};
+const Style = {
+  container: {
+    width: "100%",
+    height: "100vh",
+    display: "flex",
+    overflowY: "hidden",
+    overflowX: "hidden",
+    justifyContent: "center",
+    padding: 0,
+    flexDirection: "column"
+  },
+  gridContainer: {
+    width: "100%",
+    flexDirection: "column",
+    flex: "1",
+    display: "flex"
+  },
+  gridTop: {
+    flex: "0.7",
+    display: "flex",
+    backgroundColor: "#26CAC0"
+  },
+  gridBottom: {
+    flex: "2",
+    display: "flex",
+    backgroundColor: "#F1F1F1;"
+  },
+  gridProfil: {
+    display: "flex",
+    flexDirection: "row"
+  }
+};
 export default Style;

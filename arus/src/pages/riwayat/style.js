921c42dfd1c9d56f49c45af167aa1e2c8c49b2de
malichalaelar <maalicha.maalicha@gmail.com>
2019-11-27 17:32:04 +0700

    fix riwayat

--- arus/src/pages/riwayat/style.js
+++ arus/src/pages/riwayat/style.js
@@ -1,153 +1,62 @@
 const Style = {
-  Container: {
-    width: '100%',
-    height: '100vh',
-    backgroundSize: 'cover',
-    flexDirection: 'column',
-    display: 'flex',
-    overflowY: 'hidden',
-    overflowX: 'hidden',
-    flex: '1',
-    backgroundColor: '#F1F1F1',
-    // margin: 0
+  container: {
+    width: "100%",
     padding: 0
   },
-  gridContent: {
-    backgroundColor: 'white',
-    width: '100%',
-    height: '24%',
-    padding: 0,
-    marginTop: '11%'
+  gridTop: {
+    display: "flex",
+    backgroundColor: "#ffffff",
+    height: "139px",
+    backgroundSize: "cover"
   },
-  riwayat: {
-    fontfamily: 'Google Sans',
-    fontStyle: 'normal',
-    fontWeight: 500,
-    fontSize: '14px',
-    lineheight: '18px',
-    color: '#25282B',
-    position: 'absolute',
-    // width: '228px',
-    height: '18px',
-    left: '64px'
-    // top: '22px'
+  textTop: {
+    color: "#26CAC0",
+    fontSize: "12px"
   },
-  BoxMain: {
-    position: 'absolute',
-    width: '100%',
-    height: '139px',
-    // left: '-20px',
-    // right: '-20px',
-    // top: '57px',
-    background: '#FFFFFF'
+  itemTop: {
+    paddingLeft: "5%",
+    paddingTop: "4%"
   },
-  SedangBerjalan: {
-    // position: 'absolute',
-    // width: '106px',
-    height: '18px',
-
-    top: '22px',
-    fontfamily: 'Google Sans',
-    fontStyle: 'normal',
-    fontWeight: 500,
-    fontSize: '14px',
-    lineheight: '18px',
-    display: 'flex',
-    alignItems: 'center',
-    paddingTop: 20,
-    color: '#26CAC0',
-    backgroundColor: 'white',
-    width: '100%',
-    paddingBottom: '5%'
+  gridBox: {
+    border: "1px solid #F1F1F1",
+    height: "66px",
+    width: "90%",
+    borderRadius: "8px"
   },
-
-  BoxMiddle: {
-    // position: 'absolute',
-    // width: '100%',
-    // paddingLeft: '5%',
-    height: '66px',
-    // left: 0,
-    background: '#FFFFFF',
-    /* outline */
-
-    marginTop: 10,
-    border: '1px solid #F1F1F1',
-    boxsizing: 'border-box',
-    borderRadius: '8px',
-
-    paddingTop: '1%',
-    marginTop: 10
-  },
-  GridTop: {
-    paddingTop: '2%'
-  },
-  GridBox: {
-    alignItems: 'center'
-  },
-  gridUpper: {
-    borderRight: '1px solid #F1F1F1',
-    // borderimage: 'linear-gradient(#F1F1F1,#F1F1F1)',
-
-    borderBottomRightRadius: 40,
-    borderTopRightRadius: 40
-  },
-  gridMiddle: {
-    borderRight: '1px solid #F1F1F1',
-    borderBottomRightRadius: 40,
-    borderTopRightRadius: 40
-  },
-  gridBellow: {
-    borderRight: '1px solid #F1F1F1',
-    borderWidth: 1
+  itemBottom: {
+    display: "flex",
+    justifyContent: "center"
   },
   content: {
-    fontfamily: 'Nunito-Sans',
-    fontStyle: 'normal',
-    fontWeight: 'normal',
-    fontSize: '12px',
-    lineheight: '16px',
-    // display: 'flex',
-    alignItems: 'center',
-    color: '#9E9E9E'
+    color: "#9E9E9E",
+    fontSize: "12px"
   },
   input: {
-    fontfamily: 'Google Sans',
-    fontStyle: 'normal',
-    fontWeight: 500,
-    fontSize: '14px',
-    lineheight: '18px'
+    color: "#25282B",
+    fontSize: "14px"
   },
-  RiwayatKunjungan: {
-    fontfamily: 'Google-Sans',
-    fontStyle: 'normal',
-    fontWeight: 500,
-    fontSize: '14px',
-    lineHeight: '18px'
-    // padding: 20
+  itemContent: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+    borderRight: "1px solid #F1F1F1",
+    paddingTop: "4%"
   },
-  JumlahRiwayat: {
-    // paddingLeft: 5,
-    color: '#26CAC0',
-    fontfamily: 'Google-Sans',
-    fontStyle: 'normal',
-    fontWeight: 500,
-    fontSize: '14px',
-    lineHeight: '18px',
-    display: 'flex',
-    alignItems: 'center'
+  textBottom: {
+    color: "#25282B",
+    fontSize: "14px"
   },
-  boxBorder: {
-    borderRight: '1px solid #F1F1F1',
-    borderWidth: 1
+  itemBottomHistory: {
+    paddingTop: "5%",
+    display: "flex",
+    justifyContent: "center"
   },
-  containerContent: {
-    display: 'flex',
-    flexDirection: 'row'
-  },
-  gridContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    padding: 30
+  gridBoxBottom: {
+    border: "1px solid #F1F1F1",
+    height: "66px",
+    width: "90%",
+    borderRadius: "8px",
+    backgroundColor: "#ffffff"
   }
 };
 export default Style;

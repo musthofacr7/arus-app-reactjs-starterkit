85752ed71c1ccee1a27ecd3164cf2b1a7edf0c6b
Yuan Virna <yuvirna93@gmail.com>
2019-11-28 01:14:42 +0700

    fix detail

--- arus/src/App.js
+++ arus/src/App.js
@@ -1,15 +1,16 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import Home from "./pages/home";
-import Riwayat from "./pages/riwayat";
-import Login from "./pages/login";
-import PilihLocket from "./pages/pilihLoket";
-import SplashScreen from "./pages/splashScreen";
-import Profile from "./pages/profil";
-import EditProfile from "./pages/edit-profil";
-import ListAnggota from "./pages/listAngota";
-import EditKeluarga from "./pages/edit-anggota-keluarga";
-import AddAnggotaKeluarga from "./pages/add-anggota-keluarga";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Riwayat from './pages/riwayat';
+import Login from './pages/login';
+import PilihLocket from './pages/pilihLoket';
+import SplashScreen from './pages/splashScreen';
+import Profile from './pages/profil';
+import EditProfile from './pages/edit-profil';
+import ListAnggota from './pages/listAngota';
+import EditKeluarga from './pages/edit-anggota-keluarga';
+import AddAnggotaKeluarga from './pages/add-anggota-keluarga';
+import DetailKeluarga from './pages/detail-anggota-keluarga';
 class App extends React.Component {
   render() {
     return (
@@ -24,6 +25,8 @@ class App extends React.Component {
         <Route path="/list-anggota" exact component={ListAnggota} />
         <Route path="/add-keluarga" exact component={AddAnggotaKeluarga} />
         <Route path="/edit-profil-keluarga" exact component={EditKeluarga} />
+        <Route path="/detail-anggota-keluarga" exact component={DetailKeluarga}
+        />
       </Switch>
     );
   }

03c518f049deb360d4b93f5dbd768724f35abd8a
Yuan Virna <yuvirna93@gmail.com>
2019-11-27 21:02:41 +0700

    fab plus masih dikiri

--- arus/src/pages/listAngota/component.js
+++ arus/src/pages/listAngota/component.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Container, Typography, Grid } from '@material-ui/core';
+import { Container, Typography, Grid, Fab } from '@material-ui/core';
 import AppBar from '../../component/appbar';
 import BoxLoket from '../../component/Box-category-loket';
 import BottomNavigation from '../../component/bottom-navigation';
 import { Link } from 'react-router-dom';
 import ListData from '../../component/listAnggota';
+import AddIcon from '@material-ui/icons/Add';
 
 class component extends React.Component {
   render() {
@@ -15,28 +16,33 @@ class component extends React.Component {
         <AppBar goBack title="Anggota Keluarga" />
 
         <Grid container spacing={4} className={classes.gridUpper}>
-          {/* <Link className={classes.link} onClick={this.handleOpen}> */}
-            <Grid item>
-              <ListData />
-            </Grid>
-          {/* </Link> */}
-          {/* <Link className={classes.link} onClick={this.handleOpen}> */}
-            <Grid item>
-              <ListData />
-            </Grid>
-          {/* </Link> */}
-          {/* <Link className={classes.link} onClick={this.handleOpen}> */}
-            <Grid item>
-              <ListData />
-            </Grid>
-          {/* </Link> */}
-          {/* <Link className={classes.link} onClick={this.handleOpen}> */}
-            <Grid item>
-              <ListData />
-            </Grid>
-          {/* </Link> */}
-        </Grid>
+          <Grid item>
+            <ListData />
+          </Grid>
 
+          <Grid item>
+            <ListData />
+          </Grid>
+
+          <Grid item>
+            <ListData />
+          </Grid>
+
+          <Grid item>
+            <ListData />
+          </Grid>
+        </Grid>
+        
+          <Fab
+          align="center"
+            margin="normal"
+            style={{ backgroundColor: '#26CAC0', color: '#FFFFFF' }}
+            aria-label="add"
+            className={classes.plus}
+          >
+            <AddIcon />
+          </Fab>
+      
         <BottomNavigation />
       </Container>
     );

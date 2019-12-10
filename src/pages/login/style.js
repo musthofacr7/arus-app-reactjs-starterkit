973dbe1207ad7342c8565c063f105a5a23e52b6d
hisniaMuna <hisniamuna12@gmail.com>
2019-12-10 11:11:03 +0700

    styling fixed login

--- src/pages/login/style.js
+++ src/pages/login/style.js
@@ -1,78 +1,76 @@
 const styles = {
   container: {
-    width: '100%',
-    height: '100vh',
-    display: 'flex',
-    flex: '1',
+    width: "100%",
+    height: "100vh",
+    display: "flex",
+    flex: "1",
     padding: 0,
-    flexDirection: 'column',
-    backgroundSize: 'cover',
-    marginBottom: '8%'
+    flexDirection: "column",
+    backgroundSize: "cover",
+    marginBottom: "8%"
   },
   image: {
-    width: '100%'
+    width: "100%"
   },
   gridBottom: {
-    display: 'flex',
-    flexDirection: 'row'
+    display: "flex",
+    flexDirection: "row"
   },
   textTop: {
-    fontSize: '24px',
-    paddingLeft: '6%',
-    paddingBottom: '5%',
-    fontWeight: 'bold'
+    fontSize: "24px",
+    paddingLeft: "6%",
+    paddingBottom: "5%",
+    fontWeight: "bold"
   },
   button: {
-    fontFamily: 'Montserrat, sans-serif',
-    fontSize: '15px',
-    width: '90%',
+    fontFamily: "Montserrat, sans-serif",
+    fontSize: "15px",
+    width: "90%",
     height: 48,
-    boxShadow: '0px 0px 2px #c1b1b1',
-    border: '1px solid #F7A647',
-    borderRadius: '4px'
+    boxShadow: "0px 0px 2px #c1b1b1",
+    border: "1px solid #F7A647",
+    borderRadius: "4px"
   },
   buttonBottom: {
-    fontFamily: 'Montserrat, sans-serif',
-    fontSize: '15px',
-    width: '90%',
+    fontFamily: "Montserrat, sans-serif",
+    fontSize: "15px",
+    width: "90%",
     height: 48,
-    boxShadow: '0px 0px 2px #c1b1b1',
-    border: '1px solid #F7A647',
-    borderRadius: '4px'
+    boxShadow: "0px 0px 2px #c1b1b1",
+    border: "1px solid #F7A647",
+    borderRadius: "4px"
   },
   buttonBox: {
-    flexDirection: 'column',
-    display: 'flex',
-    alignItems: 'center'
+    flexDirection: "column",
+    display: "flex",
+    alignItems: "center"
   },
   textButton: {
-    color: '#F7A647',
-    fontFamily: 'Google Sans, sans-serif',
-    fontSize: '14px'
+    color: "#F7A647",
+    fontFamily: "Google Sans, sans-serif",
+    fontSize: "14px"
   },
   textButtonBottom: {
-    color: '#FFFFFF',
-    fontFamily: 'Google Sans, sans-serif',
-    fontSize: '14px'
+    color: "#FFFFFF",
+    fontFamily: "Google Sans, sans-serif",
+    fontSize: "14px"
   },
   gridButton: {
-    display: 'flex',
-    alignItems: 'center'
+    display: "flex",
+    alignItems: "center"
+  },
+  logo: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    color: "#FFFFFF"
   },
   buttonBoxBottom: {
-    paddingTop: '3%',
-    flexDirection: 'column',
-    display: 'flex',
-    alignItems: 'center'
-  },
-  googleLogo: {
-    paddingLeft: '40%'
-  },
-  gridIconBottom: {
-    
-    color: 'white',
-   
-  },
+    paddingTop: "3%",
+    flexDirection: "column",
+    display: "flex",
+    alignItems: "center"
+  }
 };
 
 export default styles;
